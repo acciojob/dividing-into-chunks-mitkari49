0c81a9eed1e88fe84c1c72d6b623e1a39ba9b296
mitkari49 <93242478+mitkari49@users.noreply.github.com>
2024-07-28 20:44:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  const result=[];
-	const [current_sum, current_subarray] = [0, []];
-	for (const num of arr) {
-		if(current_sum+num>n){
-			result.push(current_subarray);
-			current_sum=num;
-			current_subarray=[num];
-		}
-		else{
-			current_sum+=num; // currentsum+=num;
-			current_subarray.push(num);
-		}
-		if(current_subarray.length>0){
-		result.push(current_subarray);
-	}
-	}
-		return result;
-	}
+  const result = [];
+  let [current_sum, current_subarray] = [0, []];
+
+  for (const num of arr) {
+    if (current_sum + num > n) {
+      result.push(current_subarray);
+      current_sum = num;
+      current_subarray = [num];
+    } else {
+      current_sum += num;
+      current_subarray.push(num);
+    }
+  }
+
+  if (current_subarray.length > 0) {
+    result.push(current_subarray);
+  }
+
+  return result;
 };
 
 const n = prompt("Enter n: ");
